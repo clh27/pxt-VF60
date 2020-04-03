bb5466de366de5e6d5873147fbf1974156e630b0
clh27 <34504209+clh27@users.noreply.github.com>
2020-04-03 13:20:48 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+//% weight=100 color=#0fbc11 icon="?"
+namespace VF60 {
+    /**
+     * initialize serial display
+     * @param lösche Display
+     */
+    //% block
+    export function initDisplay(): void {
+        serial.writeString("\027\\?LD2");
+        serial.writeString("\027[2J");
+        serial.writeString("\027[1;1H");
+    }
 
+    /**
+     * initialize serial display
+     * @param col Spalte (1..20)
+     * @param row Zeile (1..2)
+     */
+    //% block
+    export function setCursor(col: number, row: number): void {
+        if (row > 0 && row < 2 && col > 0 && col < 21) {
+            serial.writeString("\027[");
+            serial.writeString(col);
+            serial.writeString(";");
+            serial.writeString(row);
+            serial.writeString("H");
+        }
+    }
+
+}
